000ff795d73793fd32cbc7606061a57d94a32587
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-09-10 17:25:51 -0500

    Update server.js to use environment variable for port configuration

--- server.js
+++ server.js
@@ -4,8 +4,10 @@
  */
 import app from "./app.js";
 import { connectDB } from "./src/config/database.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 connectDB();
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
-console.log("Server on port", 3000);
+console.log("Server on port", process.env.PORT || 3000);
